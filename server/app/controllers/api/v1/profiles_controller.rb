1195333b5bf21e3e735203edab68f096fd8692df
alvesmarcos <alves.henrique.marcos@gmail.com>
2019-12-24 17:22:28 -0300

    feat: add profiles routes
    Add model, migration and controller of profiles (read routes only)

---
+++ server/app/controllers/api/v1/profiles_controller.rb
@@ -0,0 +1,15 @@
+module Api
+  module V1
+    class ProfilesController < ApplicationController
+      def index
+        @profiles = Profile.all
+        render :json => @profiles
+      end
+    
+      def show
+        @profile = Profile.find(params[:id])
+        render :json => @profile
+      end
+    end
+  end
+end
--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+profiles = Profile.create([
+  { name: 'Administrator', slug: 'administrator', description: 'Can control all the contents of the mobile app'},
+  { name: 'Common', slug: 'common', description: 'Can access all features provide by the mobile app' },
+  { name: 'Community', slug: 'community', description: 'Can send tips and exchange messages to users that have Common as profile' }
+])
