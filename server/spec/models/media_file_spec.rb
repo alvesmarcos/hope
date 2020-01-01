b571a01fbcefc94552ad5fcb80d8bafbd82b32f3
alvesmarcos <alves.henrique.marcos@gmail.com>
2020-01-01 18:31:17 -0300

    test: remove test old models

--- server/spec/models/media_file_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe MediaFile, type: :model do
-  # Association test
-  it { should belong_to(:user) }
-end
--- server/spec/models/profile_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Profile, type: :model do
-  # Validation test
-  it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:slug) }
-end
